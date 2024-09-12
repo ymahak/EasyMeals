f12ed9ba71538aaa0f568a0590c33e463c507abb
ymahak <mehakyadav1502@gmail.com>
2024-09-12 22:28:07 +0530

    commit

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext, getAuthStatusLocalStorage } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
 import CRUDTool from "../../components/CRUDTool";
@@ -7,18 +7,25 @@ import Navbar from "../../components/Navbar";
 const ProfilePage: React.FC = () => {
   const { isLogin } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const stoAuth = getAuthStatusLocalStorage();
     if (!stoAuth || stoAuth.isLogin === "no") {
       navigate("/user/sign-in");
+    } else {
+      setLoading(false);
     }
   }, [navigate]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <>
       <Navbar />
-      {isLogin && <CRUDTool />}
+      {isLogin ? <CRUDTool /> : <p>Please log in to access your profile.</p>}
     </>
   );
 };
